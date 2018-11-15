4f686bf4adbce4ac721880de08db68039f4d2e29
Zachary Lehmann <zlehmann@gmail.com>
2018-11-15 22:20:42 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -66,7 +66,8 @@ end
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by
   # release date descending
-  Movie.where("release_date > 2002").order(':realse_date DESC')
+  test = Movie.where("release_date > 2002").order(':realse_date DESC')
+  binding.pry
 end
 
 def can_be_found_updated_and_saved
