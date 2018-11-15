fffa76c93f48eac77be3ebeb90f24d44aec8042c
Zachary Lehmann <zlehmann@gmail.com>
2018-11-15 21:37:58 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -20,7 +20,7 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = __
+  movie = Movie.create(attributes)
 end
 
 def can_be_created_in_a_block(args = __)
