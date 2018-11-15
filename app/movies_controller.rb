4439286935fb96cdb0ad203eea52246b562def8e
Zachary Lehmann <zlehmann@gmail.com>
2018-11-15 21:43:07 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -31,7 +31,7 @@ def can_be_created_in_a_block(args = {:title => "Home Alone", :release_date => 1
   # release_date == 1990
 
   Movie.create do |m|
-    binding.pry
+    Movie.create(m)
   end
 end
 
