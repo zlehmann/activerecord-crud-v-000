1c3b5a33c35b3488ac8196b3ef7909bc43579c7e
Zachary Lehmann <zlehmann@gmail.com>
2018-11-15 22:08:38 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -52,7 +52,7 @@ def can_get_size_of_the_database
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  Movie.select(:id).find(1).title
+  Movie.find_by(:id).find(1)
 end
 
 def can_find_by_multiple_attributes
