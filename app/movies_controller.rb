ea56cc800edf46fb73286ad73170bfd35a4eb69b
Zachary Lehmann <zlehmann@gmail.com>
2018-11-15 21:51:14 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -44,11 +44,11 @@ def can_get_the_first_item_in_the_database
 end
 
 def can_get_the_last_item_in_the_database
-  __
+  Movie.last
 end
 
 def can_get_size_of_the_database
-  __
+  Movie.length
 end
 
 def can_find_the_first_item_from_the_database_using_id
