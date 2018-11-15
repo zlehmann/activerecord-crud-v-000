35279d96752427aca34cb5bc6e70dcb40a827b8c
Zachary Lehmann <zlehmann@gmail.com>
2018-11-15 21:34:49 +0000

    automatically backed up by learn

--- db/migrate/20181115212954_create_movies.rb
+++ db/migrate/20181115212954_create_movies.rb
@@ -1,6 +1,6 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
-    create_table movies do |t|
+    create_table :movies do |t|
       t.string :title, :director, :lead
       t.integer :release_date
       t.boolean :in_theaters
